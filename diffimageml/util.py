a7a76969a6b37f5afd97c38398c204ca2d84fb9e
Kyle O'Connor <koconnor@justins-air-2.home>
2020-12-10 13:16:17 -0500

    more util fcns; location conversions to/from pixels/skycoords

--- diffimageml/util.py
+++ diffimageml/util.py
@@ -1,6 +1,28 @@
 import os 
 from astropy.wcs import WCS
 from astropy.nddata import Cutout2D
+from astropy.wcs.utils import skycoord_to_pixel, pixel_to_skycoord
+
+def pixtosky(self,pixel):
+    """
+    Given a pixel location returns the skycoord
+    """
+    hdu = self.hdu
+    hdr = hdu.header
+    wcs,frame = WCS(hdr),hdr['RADESYS'].lower()
+    xp,yp = pixel
+    sky = pixel_to_skycoord(xp,yp,wcs)
+    return sky
+
+def skytopix(self,sky):
+    """
+    Given a skycoord (or list of skycoords) returns the pixel locations
+    """
+    hdu = self.hdu
+    hdr = hdu.header
+    wcs,frame = WCS(hdr),hdr['RADESYS'].lower()
+    pixel = skycoord_to_pixel(sky,wcs)
+    return pixel
 
 def cut_hdu(self,location,size,writetodisk=False,saveas=None):
     """
@@ -16,9 +38,10 @@ def cut_hdu(self,location,size,writetodisk=False,saveas=None):
         
     cut = Cutout2D(dat,location,size,wcs=wcs) 
     cutwcs = cut.wcs
-    if writetodisk:
-        cphdu.data = cut.data
-        cphdu.header.update(cut.wcs.to_header())    
+    cphdu.data = cut.data
+    cphdu.header.update(cut.wcs.to_header())  
+    
+    if writetodisk:  
         cphdu.writeto(saveas,overwrite=True)
     
-    return 
+    return cphdu
