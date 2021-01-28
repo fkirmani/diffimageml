38fc6b3edf7a8636ca3eb603d5d79d7eced03445
jpierel14 <jr23@email.sc.edu>
2021-01-28 14:58:00 -0500

    globus

--- diffimageml/globus_download.py
+++ diffimageml/globus_download.py
@@ -79,6 +79,7 @@ if runProcess:
 	subprocess.call([r'./globusconnectpersonal','-setup',setup_key])
 	subprocess.Popen([r'./globusconnectpersonal','-start'],shell=False)
 	os.chdir(os.path.abspath(_filedir_))
+	print(os.path.join(os.path.abspath(_filedir_),'globusconnectpersonal-*'))
 	globus_folders = glob.glob(os.path.join(os.path.abspath(_filedir_),'globusconnectpersonal-*'))
 	print(globus_folders)
 	#for f in globus_folders:
